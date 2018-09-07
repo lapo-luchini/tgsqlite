6eb96ad7c1fbaf88ae954233378df47d0f8688c6
Lapo Luchini <lapo@lapo.it>
2018-09-07 22:42:42 +0000

    Improve schema.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --max-old-space-size=8192 --optimize-for-size
 'use strict';
 const
     fs = require('fs'),
-    sqlite = require('sqlite');
+    sqlite = require('sqlite'),
+    dateMissing = Number.MAX_SAFE_INTEGER;
 
 if (process.argv.length < 3) {
     console.log('specify path to result.json file');
@@ -14,47 +15,50 @@ if (fs.existsSync('telegram.sqlite'))
 Promise.resolve(
 ).then(() => sqlite.open('telegram.sqlite', { Promise })
 ).then(async db => {
-    for (const sql of fs.readFileSync('schema.sql', 'utf8').trim().split(/;\s+/))
+    for (const sql of fs.readFileSync('schema.sql', 'utf8').replace(/^--.*$/gm, '').trim().split(/;\s+/))
         await db.run(sql);
     await db.run('PRAGMA foreign_keys = ON');
     const chats = JSON.parse(fs.readFileSync(process.argv[2], 'utf8')).chats;
     chats.forEach(c => { // calculate age from first message
-        c.date = Date.parse(c.messages.length ? c.messages[0].date : '9999-01-01T00:00:00');
+        if (c.messages.length)
+            c.date = Date.parse(c.messages[0].date);
     });
     chats.sort((a, b) => { // sort chats by age
-        return a.date - b.date;
+        return (a.date || dateMissing) - (b.date || dateMissing);
     });
     const getUser = await db.prepare('SELECT id FROM user WHERE name = ?');
     const setUser = await db.prepare('INSERT INTO user (name) VALUES (?)');
-    const setMessage = await db.prepare('INSERT INTO message (id, chat, type, date, edited, "from", reply, "text") VALUES (?,?,?,?,?,?,?,?)');
-    const setChat = await db.prepare('INSERT INTO chat (name, type) VALUES (?,?)');
-    const reSpace = /_(supergroup|channel)$/; // these messages use a separated numbering
+    const setMessage = await db.prepare('INSERT INTO message (id, chat, type, date, edited, author, reply, text) VALUES (?,?,?,?,?,?,?,?)');
+    const setChat = await db.prepare('INSERT INTO chat (name, type, date, num) VALUES (?,?,?,?)');
+    const reSpace = /_(supergroup|channel)$/; // these messages use a separated numbering space
     let space = 0;
     for (const chat of chats) {
-        chat.id = (await setChat.run(chat.name, chat.type)).lastID;
+        chat.id = (await setChat.run(chat.name, chat.type, chat.date, chat.messages.length)).lastID;
         console.log('Chat:', {id: chat.id, name: chat.name, type: chat.type, len: chat.messages.length});
         let offset = 0;
         if (reSpace.test(chat.type)) {
+            // I assume 1E10 messages per space will be enough
+            // (this allows for 900718 spaces = Number.MAX_SAFE_INTEGER / 1E10)
             ++space;
-            offset = space * 10000000000; // I assume 1E10 messages per space will be enough (this allows for 900718 spaces = Number.MAX_SAFE_INTEGER / 1E10)
+            offset = space * 1E10;
         }
         await db.run('BEGIN');
         for (const m of chat.messages) {
-            let from = m.from;
-            if (from) {
-                from = await getUser.get(m.from);
-                if (from)
-                    from = from.id;
+            let author = m.from;
+            if (author) {
+                author = await getUser.get(m.from);
+                if (author)
+                    author = author.id;
                 else {
                     let st = await setUser.run(m.from);
-                    from = st.lastID;
+                    author = st.lastID;
                 }
             }
             await setMessage.run(m.id + offset, chat.id,
                 (m.type == 'message') ? null : m.type,
                 Date.parse(m.date),
-                Date.parse(m.edited),
-                from,
+                m.edited.startsWith(1970) ? null : Date.parse(m.edited),
+                author,
                 m.reply_to_message_id,
                 JSON.stringify(m.text));
         }
--- schema.sql
+++ schema.sql
@@ -1,24 +1,26 @@
 CREATE TABLE chat (
-    "id" INTEGER PRIMARY KEY,
-    "name" TEXT,
-    "type" TEXT,
-    "date" INTEGER
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    type TEXT,
+    date INTEGER, -- taken from first message
+    num INTEGER -- total number of messages
 );
 CREATE TABLE user (
-    "id" INTEGER PRIMARY KEY,
-    "name" TEXT
+    id INTEGER PRIMARY KEY,
+    name TEXT
 );
-CREATE INDEX user_name ON user ("name");
+CREATE INDEX user_name ON user (name); -- used to find existing users during import
 CREATE TABLE message (
-    "id" INTEGER PRIMARY KEY,
-    "chat" INTEGER,
-    "type" TEXT, -- NULL is the default "message" type
-    "date" INTEGER,
-    "edited" INTEGER,
-    "from" INTEGER,
-    "reply" INTEGER, -- "reply_to_message_id" field
-    "text" TEXT, -- in JSON as it can contain formatting
-    FOREIGN KEY ("chat") REFERENCES chat ("id"),
-    FOREIGN KEY ("from") REFERENCES user ("id")
+    id INTEGER PRIMARY KEY,
+    chat INTEGER,
+    type TEXT, -- NULL for the default "message" type
+    date INTEGER,
+    edited INTEGER, -- is NULL when dump contains year 1970
+    author INTEGER, -- "from" field
+    reply INTEGER, -- "reply_to_message_id" field
+    text TEXT, -- in JSON as it can contain formatting
+    FOREIGN KEY (chat) REFERENCES chat (id),
+    FOREIGN KEY (author) REFERENCES user (id)
 );
-CREATE INDEX message_chat ON message ("chat", "id");
+-- can be useful, but it does occupy space and can always be created later
+-- CREATE INDEX message_chat ON message (chat, id)
